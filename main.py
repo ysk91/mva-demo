db2acfc265f15fc882b8a08c13f6e4f559b0be63
ysk91 <ysk91.engineer@gmail.com>
2024-09-30 22:45:03 +0900

    プロンプトをJSONモードに最適化

--- main.py
+++ main.py
@@ -13,23 +13,26 @@ zcode = input('郵便番号を入力してください: ')
 address = zipcode.get(zcode).json()
 
 prompt = f"""
-{address}のエリアに応じて出力を変えてください。
-ただし、出力は必ず1文字の数字であること。
-エリア: 出力する値 の対応表は以下です。
+{address}のエリアに応じて<<area>>をjson形式で出力してください。
+ただし、<<area>>は必ず1文字の数字であること。
+エリア: <<area>> の対応表は以下です。
 関東: 1,
 関西: 2,
 その他: 3
+
+[出力項目]
+area: <<area>>
 """
 
 gpt_responce = gpt.post(prompt, temperature=0.0)
-area = gpt.content(gpt_responce)
+area = int(gpt.content(gpt_responce)['area'])
 
 try:
-    if area == '1':
+    if area == 1:
         print('関東〜〜〜〜〜！！！')
-    elif area == '2':
+    elif area == 2:
         print('関西〜〜〜〜〜！！！')
-    elif area == '3':
+    elif area == 3:
         area / 0 # ゼロ除算エラーを発生させる
 except Exception as e:
     script_path = os.path.basename(__file__)
