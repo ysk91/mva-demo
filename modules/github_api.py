cd3b1ceaff6975c7828ff9b4742aa312db06a768
ysk91 <ysk91.engineer@gmail.com>
2024-10-20 09:31:51 +0900

    checkout動作を修正

--- modules/github_api.py
+++ modules/github_api.py
@@ -46,6 +46,10 @@ LOCAL_REPO_PATH = get_local_repo_path()
 local_repo = Repo(LOCAL_REPO_PATH)
 
 
+def get_current_branch():
+    return local_repo.active_branch.name
+
+
 def checkout_and_pull(branch_name):
     try:
         local_repo.remotes.origin.fetch()
@@ -55,15 +59,21 @@ def checkout_and_pull(branch_name):
         print(f"Error checking out and pulling {branch_name}: {e}")
 
 
-def commit_and_push_to_branch(branch_name, commit_message):
-    current_branch = local_repo.active_branch.name
+def specific_file_checkout(branch_name, target_file):
+    try:
+        local_repo.remotes.origin.fetch()
+        #  指定したファイル以外に影響が出ないように、checkoutするファイルを指定
+        local_repo.git.checkout(f'origin/{branch_name}', '--', target_file)
+        print(f"Successfully checked out {target_file} from {branch_name}")
+    except Exception as e:
+        print(f"Error checking out {target_file} from {branch_name}: {e}")
 
-    local_repo.git.add("foods.yml")
+
+def commit_and_push_to_branch(branch_name, commit_message, file_path):
+    local_repo.git.add(file_path)
     local_repo.index.commit(commit_message)
 
     origin = local_repo.remote(name="origin")
     origin.push(f"HEAD:refs/heads/{branch_name}")
 
     print(f"Changes pushed to {branch_name}")
-
-    checkout_and_pull(current_branch)
